19eae66f18f88356b3064a519945e35cb26e1a44
codeesura <120671243+codeesura@users.noreply.github.com>
2022-12-15 20:52:15 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+
+client.run('TOKEN')
